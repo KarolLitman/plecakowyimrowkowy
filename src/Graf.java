77834f0f928b3784a33b3fdeeddbfcf077028ba8
kk9056 <45825945+kk9056@users.noreply.github.com>
2018-12-17 11:21:42 +0100

    sss

--- src/Graf.java
+++ src/Graf.java
@@ -11,12 +11,13 @@ class przedmiot{
     String nazwa;
     double masa;
     double cena;
-
+    double stosunek;
     przedmiot(String nazwa,double masa, double cena) {
 
     this.nazwa=nazwa;
     this.masa=masa;
     this.cena=cena;
+    stosunek=oblicz_stosunek();
     }
 
     double oblicz_stosunek(){
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-
+	
     public static void main(String[] args) {
+    	problem_plecakowy p=new problem_plecakowy();
+    	p.lista_przedmiotow.add((new przedmiot("statu쿮tka",20,100)));
+    	p.lista_przedmiotow.add((new przedmiot("statu쿮tka1",40,650)));
+    	p.lista_przedmiotow.add((new przedmiot("statu쿮tka2",30,450)));
+    	p.lista_przedmiotow.add((new przedmiot("statu쿮tka3",10,400)));
+    	p.lista_przedmiotow.add((new przedmiot("statu쿮tka4",60,650)));
         System.out.println("Hello World!");
     }
 }
